6bb9755e2d18a5ea267dbceb2c4f8710767157bd
Sayed-Ariful-Hoque <sayedarifulhoque171016xyz@gmail.com>
2025-08-13 19:51:44 +0700

    event mangagement company Hompage design

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -16,3 +16,11 @@ $(document).ready(function(){
 });
 
 // Hero Area Slider JS End
+
+// Fancybox JS Start
+Fancybox.bind("[data-fancybox='photo-gallery-wrap']", {
+  animated: true,
+  showClass: "fancybox-fadeIn",
+  hideClass: "fancybox-fadeOut",
+});
+// Fancybox JS End
