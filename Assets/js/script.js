f4c197577387d651695bddc44b1ab15186fa50b5
Sayed-Ariful-Hoque <sayedarifulhoque171016xyz@gmail.com>
2025-08-25 19:17:02 +0700

    slider designe

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,14 +1,13 @@
-
 // Hero Area Slider JS Start
-$(document).ready(function(){
-  $('.hero-slider').slick({
-    autoplay: true,
-    autoplaySpeed: 4500,
-    speed: 1000,
+$(document).ready(function () {
+  $(".hero-slider").slick({
+    // autoplay: true,
+    // autoplaySpeed: 4500,
+    // speed: 1000,
     dots: false,
     arrows: true,
     fade: true,
-    cssEase: 'ease-in-out',
+    cssEase: "ease-in-out",
     pauseOnHover: false,
     pauseOnFocus: false,
     adaptiveHeight: false,
@@ -16,9 +15,7 @@ $(document).ready(function(){
 });
 
 // Hero Area Slider JS End
-
-// Fancybox JS Start
-Fancybox.bind("[data-fancybox='photo-gallery-wrap']", {
+Fancybox.bind("[data-fancybox]", {
   animated: true,
   showClass: "fancybox-fadeIn",
   hideClass: "fancybox-fadeOut",
@@ -27,9 +24,21 @@ Fancybox.bind("[data-fancybox='photo-gallery-wrap']", {
 
 // section Animation  Js Start
 AOS.init({
-    once: true,          
-    offset: 100,
-    duration: 1200,
-    easing: 'ease-in-out'
+  once: true,
+  offset: 100,
+  duration: 1200,
+  easing: "ease-in-out",
 });
 // section Animation  Js End
+
+// swiper slider
+var swiper = new Swiper(".mySwiper", {
+  direction: "vertical",
+  slidesPerView: 1,
+  spaceBetween: 30,
+  mousewheel: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
